df919090429e0e61cf7c66ba57a538ed8909241e
James Pelster <jpmac26@gmail.com>
2021-10-25 12:37:28 -0700

    Tweak ReadNullTerminatedString formatting

--- DRV3-Sharp-Library/Utils.cs
+++ DRV3-Sharp-Library/Utils.cs
@@ -23,11 +23,10 @@ namespace DRV3_Sharp_Library
 {
     public static class Utils
     {
-        // Annoyingly, there's no easy way to read a null-terminated ASCII string in .NET
+        // Annoyingly, there's no easy way to read a null-terminated string in .NET
         // (or maybe I'm just a moron), so we have to do it manually.
         public static string ReadNullTerminatedString(BinaryReader reader, Encoding encoding)
         {
-            
             using BinaryReader stringReader = new(reader.BaseStream, encoding, true);
             StringBuilder sb = new();
             while (stringReader.BaseStream.Position < stringReader.BaseStream.Length)
@@ -35,8 +34,7 @@ namespace DRV3_Sharp_Library
                 char c = stringReader.ReadChar();
 
                 // Break on null terminator
-                if (c == 0)
-                    break;
+                if (c == 0) break;
 
                 sb.Append(c);
             }
